c69a3f2eca8651287eae5774f7259b4e4fc4a35b
liumi <liu.mi@whaley.cn>
2019-03-26 10:10:36 +0800

    接口请求改造

--- src/axios/setting.js
+++ src/axios/setting.js
@@ -7,14 +7,15 @@ const $http = axios.create({
   timeout: 10000 // 请求超时时间
 })
 
-$http.defaults.baseURL = 'http://localhost:8001/'
+$http.defaults.baseURL = 'http://localhost:8888/'
 
 $http.interceptors.request.use(config => {
   config.headers = {
     'Content-Type': 'application/json;charset=UTF-8'
   }
   if (sessionStorage.getItem('token')) {
-    config.headers.Authorization = 'Bearer ' + sessionStorage.getItem('token')
+    const token = sessionStorage.getItem('token')
+    config.headers['Authorization'] = `Bearer ${token}`
   }
   return config
 }, err => {
@@ -25,15 +26,18 @@ $http.interceptors.response.use(
   function (response) {
     return Promise.resolve(response)
   },
-
   function (error) {
-    switch (error.response.status) {
+    /*switch (error.response.status) {
       case 401:
         ElementUI.Message.error('你无此操作权限')
         break
       case 403:
         router.push({path: '/login'})
-        break
+        break*/
+
+        //}
+    error.request.onerror = function (err) {
+      alert(err.toString())
     }
     return Promise.reject(error)
   }
